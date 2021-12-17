22e695963332dc4b648caf1618d4f012b5f933e1
Ankan Ghosh <ankanghosh010@gmail.com>
2021-12-17 15:55:35 +0530

    Maps added with custom marker

--- app/screens/Map.js
+++ app/screens/Map.js
@@ -1,14 +1,93 @@
-import React from 'react'
-import { View, Text } from 'react-native';
+import React, { useState } from 'react'
+import { SafeAreaView, Text, Image, StyleSheet, Dimensions, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import MapView, { Callout, Marker } from 'react-native-maps';
 
-function Mapscreen() {
+export default function Mapscreen() {
+
+    const [pin, setPin] = useState({
+        latitude: 22.6258,
+        longitude: 88.4348,
+
+    })
     return (
-        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-            <Text style={{ fontSize: 25 }}>Map Screen</Text>
-        </View>
+        <SafeAreaView forceInset={{ top: 'always' }}>
+            <MapView
+                style={styles.map}
+                loadingEnabled={true}
+                initialRegion={{
+                    longitude: 88.3639,
+                    latitude: 22.5722,
+                    latitudeDelta: 0.03,
+                    longitudeDelta: 0.05,
+                }}
+            >
+                <MapView.Marker
+                    coordinate={{
+                        latitude: 22.6258,
+                        longitude: 88.4348,
+                    }}
+                    onSelect={() => console.log('onSelect', arguments)}
+                    onDrag={() => console.log('onDrag', arguments)}
+                    onDragStart={() => console.log('onDragStart', arguments)}
+                    draggable
+                >
+                    <Image
+                        style={{ width: 50, height: 32 }}
+                        source={require('../../assets/map.png')} resizeMode='cover' />
+
+                </MapView.Marker>
+
+                <MapView.Marker
+                    coordinate={{
+                        longitude: 88.3639,
+                        latitude: 22.5722,
+                    }}
+                    onSelect={() => console.log('onSelect', arguments)}
+                    onDrag={() => console.log('onDrag', arguments)}
+                    onDragStart={() => console.log('onDragStart', arguments)}
+                    draggable
+                >
+                    <View>
+                        <Image
+                            style={{ width: 50, height: 32 }}
+                            source={require('../../assets/hospital.png')} 
+                            resizeMode='cover'
+                        />                
+                    </View>
+                    <Callout style={styles.callout}>
+                        <ScrollView>
+                        <Text>test testtest testtest testtest testtest testtest test</Text>
+                        </ScrollView>
+
+                    </Callout>
+                   
+                </MapView.Marker>
+            </MapView>
+        </SafeAreaView>
     )
 }
-export default Mapscreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 22
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+    callout:{
+        backgroundColor:'#fff',
+        padding:'10%',
+        alignItems:'center',
+        borderRadius:50
+    }
+});
 
 
-// home cart  map profile menu 
+
+// home cart  map profile menu s
